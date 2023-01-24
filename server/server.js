b9bdde393e3581f1f6c3c9b8aaa184a7b92becde
Robert <109295306+rarellano5280@users.noreply.github.com>
2023-01-23 20:58:33 -0700

    exporting typedefs and resolvers

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
+
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth')
@@ -15,8 +16,6 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware,
 });
-
-server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
